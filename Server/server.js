3219d2b63359dbbd973215370bf93c8ee9753f6c
migue <miguel.sanchezprogrammer@gmail.com>
2020-01-08 17:56:34 +0100

    Basic Rest Server

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+require("./config/config.js")
+const express = require("express") ;
+const app = express();
+const body_parser = require("body-parser");
+const port = process.env.port;
+
+app.use(body_parser.urlencoded({extended:false}));
+app.use(body_parser.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello world");
+});
+
+app.get('/usuario',(req,res)=>{
+    res.send("GET");
+});
+
+app.post('/usuario',(req,res)=>{
+    let body = req.body;
+    console.log(req.body);
+    res.json(
+        {body}
+    );
+});
+
+app.put('/usuario',(req,res)=>{
+    res.send("PUT");
+});
+
+app.delete('/usuario',(req,res)=>{
+    res.send("DELETE");
+});
+
+app.listen(port,() =>{
+    console.log(`Escuchango en el puerto: ${port}`);
+})
